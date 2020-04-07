21dc5316f438b4304cfbee069efea378d243de06
Mingxun Wang <mwang87@gmail.com>
2020-04-07 12:41:26 -0700

    using gunicorn

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python ./app.py
-#gunicorn -w 4 -b 0.0.0.0:5000 --timeout 3600 app:server --access-logfile /app/logs/access.log
+#python ./app.py
+gunicorn -w 4 -b 0.0.0.0:5000 --timeout 3600 app:server --access-logfile /app/logs/access.log
