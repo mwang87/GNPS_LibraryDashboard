5e8da95ef459d4fbb0526df877b562a8e79c790a
Mingxun Wang <mwang87@gmail.com>
2022-11-28 15:04:40 -0800

    updating linkout

--- app.py
+++ app.py
@@ -186,12 +186,16 @@ CONTRIBUTORS_DASHBOARD = [
     dbc.CardHeader(html.H5("Contributors")),
     dbc.CardBody(
         [
-            "Mingxun Wang PhD - UC San Diego",
+            "Mingxun Wang PhD - UC Riverside",
             html.Br(),
             html.Br(),
             html.H5("Citation"),
             html.A('Mingxun Wang, Jeremy J. Carver, Vanessa V. Phelan, Laura M. Sanchez, Neha Garg, Yao Peng, Don Duy Nguyen et al. "Sharing and community curation of mass spectrometry data with Global Natural Products Social Molecular Networking." Nature biotechnology 34, no. 8 (2016): 828. PMID: 27504778',
-                   href="https://www.nature.com/articles/nbt.3597")
+                   href="https://www.nature.com/articles/nbt.3597"),
+            html.Br(),
+            html.Br(),
+            html.A('Checkout our other work!', 
+                href="https://www.cs.ucr.edu/~mingxunw/")
         ]
     )
 ]
